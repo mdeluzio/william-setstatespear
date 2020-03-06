3dfecebf8a246c5406a97b92fdcf75d27b4c2316
Mark DeLuzio <deluziomark@gmail.com>
2020-03-05 20:17:10 -0500

    updated application to use Route components to dynamically display content

--- src/PoemListPage.js
+++ src/PoemListPage.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import POEMS from './poems'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import POEMS from './poems';
 
 export default function PoemListPage() {
   return (
@@ -8,9 +9,9 @@ export default function PoemListPage() {
       <ul className='PoemList'>
         {POEMS.map(poem =>
           <li key={poem.id}>
-            <a href={`/poem/${poem.id}`}>
+            <Link to={`/poem/${poem.id}`}>
               {poem.title}
-            </a>
+            </Link>
           </li>
         )}
       </ul>
